5d2557cfecbb7c89a43a3bff5e411cf40ab8e8a3
Russell Van Curen <russellvancuren@Russells-Laptop.local>
2023-12-05 05:48:37 -0500

    test

--- services/bucketFunctions.ts
+++ services/bucketFunctions.ts
@@ -4,9 +4,9 @@ import { Blob, R2Bucket, ReadableStream } from "@cloudflare/workers-types";
 export async function getBucketData<T>(key: string, defaultValue: T[] = []) {
   "use server";
   // Get all the videos from the Cloudflare R2 API
-  const memorialBucket = binding<R2Bucket>("memorialBucket");
+  const vcgMemorialBucket = binding<R2Bucket>("vcg-memorial-bucket");
 
-  const fileContents = await memorialBucket.get(key);
+  const fileContents = await vcgMemorialBucket.get(key);
 
   // If there are no videos, return an empty array
   if (!fileContents) return defaultValue;
@@ -17,30 +17,30 @@ export async function getBucketData<T>(key: string, defaultValue: T[] = []) {
 
 export async function appendBucketData<T>(key: string, newData: T) {
   // Get all the data from the R2 API
-  const memorialBucket = binding<R2Bucket>("memorialBucket");
-  const data = await memorialBucket.get(key);
+  const vcgMemorialBucket = binding<R2Bucket>("vcg-memorial-bucket");
+  const data = await vcgMemorialBucket.get(key);
 
   // Add the new data to the array
   const newArray = [...JSON.parse((await data?.text()) ?? "[]"), newData];
 
   // Save the new videos array to the R2 store
-  await memorialBucket.put(key, JSON.stringify(newArray));
+  await vcgMemorialBucket.put(key, JSON.stringify(newArray));
 }
 
 export async function writeBucketData<T>(
   key: string,
   value: ReadableStream | ArrayBuffer | ArrayBufferView | string | null | Blob,
 ) {
-  const memorialBucket = binding<R2Bucket>("memorialBucket");
-  await memorialBucket.put(key, value);
+  const vcgMemorialBucket = binding<R2Bucket>("vcg-memorial-bucket");
+  await vcgMemorialBucket.put(key, value);
 }
 
 export async function deleteBucketData(key: string) {
-  const memorialBucket = binding<R2Bucket>("memorialBucket");
-  await memorialBucket.delete(key);
+  const vcgMemorialBucket = binding<R2Bucket>("vcg-memorial-bucket");
+  await vcgMemorialBucket.delete(key);
 }
 
 export async function getRawBucketData<T>(key: string) {
-  const memorialBucket = binding<R2Bucket>("memorialBucket");
-  return await memorialBucket.get(key);
+  const vcgMemorialBucket = binding<R2Bucket>("vcg-memorial-bucket");
+  return await vcgMemorialBucket.get(key);
 }
